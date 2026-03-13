2d5a2c64c22bcc02ae80a001ead952924d59bdf4
Matthew Belanger <msb64@njit.edu>
2026-03-13 19:03:30 -0400

    main.py: Invalid image issue

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def generate_qr_code(data, path, fill_color='red', back_color='white'):
         qr = qrcode.QRCode(version=1, box_size=10, border=5)
         qr.add_data(data)
         qr.make(fit=True)
+        logging.info(f'Creating QR code with fill_color = {fill_color}, back_color = {back_color}')
         img = qr.make_image(fill_color=fill_color, back_color=back_color).convert('RGB')
 
         with path.open('wb') as qr_file:
